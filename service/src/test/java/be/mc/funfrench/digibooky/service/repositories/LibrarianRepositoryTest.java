d3aef9bd5accd481c24b894d7e52c819a794e700
fracresc <fra.cresc@gmail.com>
2019-10-28 14:54:02 +0100

    LibraryRepo added

---
+++ service/src/test/java/be/mc/funfrench/digibooky/service/repositories/LibrarianRepositoryTest.java
@@ -0,0 +1,21 @@
+package be.mc.funfrench.digibooky.service.repositories;
+
+import be.mc.funfrench.digibooky.domain.users.Librarian;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibrarianRepositoryTest {
+
+//    @Test
+//    void persist_givenLibrarian_returnCorrectLibrarian() {
+//        LibrarianRepository librarianRepo = new LibrarianRepository();
+//        Librarian librarian = new Librarian("1234", "Pino", "Pinelli", "pino@gmail.com");
+//
+//        Assertions.assertEquals(librarian, librarianRepo.persist(librarian));
+//
+//    }
+}
